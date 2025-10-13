88636c61f3719a9a39a81f83e0586aade6b81338
Fabien Sanglard <fabien.sanglard@gmail.com>
2025-10-13 11:08:17 -0700

    Upgrade dependency to window-sys v0.61.2

--- src/windows.rs
+++ src/windows.rs
@@ -256,7 +256,7 @@ impl WindowsIfChangeNotifier {
                 AF_UNSPEC,
                 Some(if_change_callback),
                 ret.tx as *const c_void,
-                0,
+                false,
                 &mut ret.handle,
             )
         };
